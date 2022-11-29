a326e88b74bf58d4e116106e3d3a835706b6d5ed
Andrey <andreytimakhovich@hp.localdomain>
2022-11-29 14:20:16 +0300

    implement dynamic adding menu cards using data goten from json db

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -155,10 +155,10 @@ window.addEventListener('DOMContentLoaded', () => {
   // Menu card
 
   class MenuCard {
-    constructor(imgSrc, imgAlt, subtitle, descr, price, parentSelector) {
-      this.imgSrc = imgSrc;
-      this.imgAlt = imgAlt;
-      this.subtitle = subtitle;
+    constructor(img, altimg, title, descr, price, parentSelector) {
+      this.img = img;
+      this.altimg = altimg;
+      this.title = title;
       this.descr = descr;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
@@ -173,8 +173,8 @@ window.addEventListener('DOMContentLoaded', () => {
     render() {
       const cardHTML = `
             <div class="menu__item">
-              <img src=${this.imgSrc} alt=${this.imgAlt}>
-              <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+              <img src=${this.img} alt=${this.altimg}>
+              <h3 class="menu__item-subtitle">${this.title}</h3>
               <div class="menu__item-descr">${this.descr}</div>
               <div class="menu__item-divider"></div>
               <div class="menu__item-price">
@@ -188,31 +188,22 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  new MenuCard(
-    'img/tabs/vegy.jpg',
-    'vegy', 'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container'
-  ).render();
+  const getResource = async url => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    25,
-    '.menu .container'
-  ).render();
+    if (!res.ok ) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
 
-  new MenuCard(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    18,
-    '.menu .container'
-  ).render();
+    return await res.json();
+  };
+
+  getResource('http://localhost:3000/menu')
+    .then(data => {
+      data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
 
   // Forms
 
@@ -225,10 +216,22 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   forms.forEach(form => {
-    postData(form);
+    bindPostData(form);
   })
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
   
-  function postData(form) {
+  function bindPostData(form) {
     form.addEventListener('submit', (event) => {
       event.preventDefault();
 
@@ -242,22 +245,9 @@ window.addEventListener('DOMContentLoaded', () => {
       form.insertAdjacentElement('afterend', statusMessage);
 
       const formData = new FormData(form);
-      const object = {};
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      formData.forEach(function(value, key) {
-        object[key] = value;
-      })
-
-      const json = JSON.stringify(object);
-
-      fetch('server.php', {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      })
-      .then(data => data.text())
+      postData('http://localhost:3000/requests', json)
       .then(data => {
         console.log(data);
         showThanksModal(message.success);
@@ -295,9 +285,4 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   };
-
-  fetch('http://localhost:3000/menu')
-    .then(data => data.json())
-    .then(res => console.log(res));
-    
 });
